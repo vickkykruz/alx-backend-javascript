a2bc0e590b144fc05aab28a4818ca700d6684265
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-05 15:38:59 -0800

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-  // eslint-disable-next-line no-unused-vars
-    (response) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    },
-    (error) => {
-      console.error(error);
-      return new Error();
-    },
-  );
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
