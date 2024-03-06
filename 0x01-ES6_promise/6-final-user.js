1afab80e85a70fe74a3faad4713a3ecf986f19fa
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-06 00:10:10 -0800

    Testing

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise = [
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promise)
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  ]).then(([signUpResponse, uploadPhotoResponse]) => [
+    {
+      status: signUpResponse.status,
+      value: signUpResponse.status === 'fulfilled' ? signUpResponse.value : signUpResponse.reason,
+    },
+    {
+      status: uploadPhotoResponse.status,
+      value: uploadPhotoResponse.status === 'fulfilled' ? uploadPhotoResponse.value : String(uploadPhotoResponse.reason),
+    },
+  ]);
 }
