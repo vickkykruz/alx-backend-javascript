65d676ad5dfa094c5b423708e0bda5bbfd3bd79c
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-07 20:59:26 +0000

    A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    if (
+      typeof this.evacuationWarningMessage !== 'function'
+    && this.constructor !== Building
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
+    this._sqft = sqft;
+  }
+}
