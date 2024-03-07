d5dd78a2dcf95fbdf2ed661e84ada076912fbf79
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-07 21:43:09 +0000

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+
+    this._amount = amount;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
