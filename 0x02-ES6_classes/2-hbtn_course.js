827501f30f1803b92f3446d57627855e39c1dbda
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-07 18:23:26 +0000

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw new TypeError('Lenght must be a number');
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
+    students.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw new Error('Each student must be a string');
+      }
+    });
+    this._students = students;
+  }
+}
