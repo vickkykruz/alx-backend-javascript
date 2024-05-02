866cedf0a85bb41198ba5ac8c6bac4f97b5a4393
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-05-02 20:11:04 +0100

    Changes

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,15 +1,15 @@
-const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
 
-describe('getPaymentTokenFromAPI', () => {
-  describe('check if arg true', () => {
-    it('resolved correctly', () => new Promise((done) => {
+describe('getPaymentTokenFromAPI', function () {
+  describe('arg true', function () {
+    it('resolved correctly', function (done) {
       getPaymentTokenFromAPI(true)
         .then((res) => {
           expect(res).to.include({ data: 'Successful response from the API' });
           done();
         })
         .catch((err) => done(err));
-    }));
+    });
   });
 });
