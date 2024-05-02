d96ab825690052377d7518887522a2014dc8723d
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-05-02 13:34:41 +0000

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  describe('check if arg true', () => {
+    it('resolved correctly', () => new Promise((done) => {
+      getPaymentTokenFromAPI(true)
+        .then((res) => {
+          expect(res).to.include({ data: 'Successful response from the API' });
+          done();
+        })
+        .catch((err) => done(err));
+    }));
+  });
+});
